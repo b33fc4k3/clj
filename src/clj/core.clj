24d953d1bfee26a7d465e204c1b6789958644642
b33fc4k3 <kali.84@gmx.net>
2013-10-01 13:01:59 +0200

    some test functions

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -1,6 +1,20 @@
-(ns clj.core)
+(ns clj.core
+  (require [net.cgrand.enlive-html :as html])
+  (:use clojure.algo.monads))
 
+; (foo "blub")
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+; as seen here: http://onclojure.com/2009/03/05/a-monad-tutorial-for-clojure-programmers-part-1/
+; (macroexpand-1 '(domonad identity-m [a 1 b (inc a)] (* a b)))
+(domonad identity-m
+  [a  1
+   b  (inc a)]
+  (* a b))
+
+; (parse-tds "http://127.0.0.1")
+(defn parse-tds [url] 
+ (html/select (html/html-resource (java.net.URL. url)) [:li]))
